55055ccfefafab8d16dad6d0b0e672e8fe84d12f
Dima Kogan <dima@secretsauce.net>
2015-10-05 22:34:40 -0700

    record script takes a timeout argument

--- record_alloc.sh
+++ record_alloc.sh
@@ -2,4 +2,7 @@
 
 source reademacsvar.sh
 
-sudo perf record ${=RECORD_OPTS} -g --call-graph=fp -p ${EMACS_PID} -eprobe_libc:{free,{malloc,calloc,realloc}{,_ret}} -eprobe_libc:aligned_alloc{,_1,_ret} -eprobe_libc:posix_memalign{,_ret}
+# t is the first argument, if given, otherwise 20. This is the timeout, in seconds
+t=${1:-20}
+
+sudo timeout $t perf record ${=RECORD_OPTS} -g --call-graph=fp -p ${EMACS_PID} -eprobe_libc:{free,{malloc,calloc,realloc}{,_ret}} -eprobe_libc:aligned_alloc{,_1,_ret} -eprobe_libc:posix_memalign{,_ret}
