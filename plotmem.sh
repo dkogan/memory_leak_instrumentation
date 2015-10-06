7f0a22d30dca4a466d2c0df63a3f1bdf836d20e5
Dima Kogan <dima@secretsauce.net>
2015-10-05 22:09:27 -0700

    memory plot labels the axes

--- plotmem.sh
+++ plotmem.sh
@@ -2,4 +2,5 @@
 
 source reademacsvar.sh
 
-while (true) { ps -h -p $EMACS_PID -O rss; usleep 250000 } | awk '{print NR/4.,$2; fflush();}' | tee /tmp/memory.tmp.log | feedgnuplot --stream --lines --domain
+while (true) { ps -h -p $EMACS_PID -O rss; usleep 250000 } | awk '{print NR/4.,$2; fflush();}' | tee /tmp/memory.tmp.log | \
+          feedgnuplot --stream --lines --domain --xlabel "Time (s)" --ylabel "Memory usage (kB)"
