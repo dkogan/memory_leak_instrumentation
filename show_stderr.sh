122fe4b4a0b755780bed309e6fd81b89e98bca4e
Dima Kogan <dima@secretsauce.net>
2015-10-05 22:01:12 -0700

    added tool to log emacs stderr

---
+++ show_stderr.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+source reademacsvar.sh
+
+# read STDERR output, join single characters into lines, tee to a log file,
+# separate temporal lulls with newlines
+sudo unbuffer sysdig proc.name=${EMACS_CMD:t} -c stderr |& \
+    perl -ne 'BEGIN { autoflush STDOUT; } if(length($_) == 2) { chomp; } print' | \
+    tee /tmp/emacs.sysdig.log | \
+    perl -M'Time::HiRes qw(gettimeofday tv_interval)' -pe '@t1 = gettimeofday; @t0 = @t1 unless @t0; $dt = tv_interval(\@t0, \@t1); print "\n" if $dt > 0.1; @t0 = @t1'
