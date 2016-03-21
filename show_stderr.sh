d14f4d72b69988a9995112aef16eedfa36912646
Dima Kogan <dima@secretsauce.net>
2016-03-21 16:02:24 -0700

    autoflush

--- show_stderr.sh
+++ show_stderr.sh
@@ -7,4 +7,4 @@ source reademacsvar.sh
 sudo unbuffer sysdig proc.name=${EMACS_CMD:t} -c stderr |& \
     perl -ne 'BEGIN { autoflush STDOUT; } if(length($_) == 2) { chomp; } print' | \
     tee /tmp/emacs.sysdig.log | \
-    perl -M'Time::HiRes qw(gettimeofday tv_interval)' -pe '@t1 = gettimeofday; @t0 = @t1 unless @t0; $dt = tv_interval(\@t0, \@t1); print "\n" if $dt > 0.1; @t0 = @t1'
+    perl -M'Time::HiRes qw(gettimeofday tv_interval)' -pe 'BEGIN { autoflush STDOUT;} @t1 = gettimeofday; @t0 = @t1 unless @t0; $dt = tv_interval(\@t0, \@t1); print "\n" if $dt > 0.1; @t0 = @t1'
