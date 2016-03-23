5381d576c624139cc2516e45daf72cbc8708e337
Dima Kogan <dima@secretsauce.net>
2016-03-23 15:43:30 -0700

    emacs now assumed to live in ~/emacs, not /tmp/...

--- reademacsvar.sh
+++ reademacsvar.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-EMACS_CLIENT_CMD=/tmp/emacsclient-tst
-EMACS_CMD=/tmp/emacs-tst
+EMACS_CLIENT_CMD_RAW=/home/dima/emacs/lib-src/emacsclient
+EMACS_CMD_RAW=/home/dima/emacs/src/emacs
+EMACS_CMD=${EMACS_CMD_RAW:h}/emacs-tst
+EMACS_CLIENT_CMD=${EMACS_CLIENT_CMD_RAW:h}/emacsclient-tst
 EMACS_PID=`pidof ${EMACS_CMD:t}`
 
 # Used by emacs for its daemon pipe. I override this here so that I can have an
@@ -12,3 +14,5 @@ export TMPDIR=/tmp/emacstest
 # Some options for 'perf record'
 RECORD_OPTS='-m512 -r50'
 
+test -e $EMACS_CMD        || ln -fs $EMACS_CMD_RAW        $EMACS_CMD
+test -e $EMACS_CLIENT_CMD || ln -fs $EMACS_CLIENT_CMD_RAW $EMACS_CLIENT_CMD
