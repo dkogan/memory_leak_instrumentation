0e463a12d2ba3e42cc388916afdad65775128e26
Dima Kogan <dima@secretsauce.net>
2016-03-31 15:28:02 -0700

    record: use all the available events

--- record_alloc.sh
+++ record_alloc.sh
@@ -5,4 +5,4 @@ source reademacsvar.sh
 # t is the first argument, if given, otherwise 20. This is the timeout, in seconds
 t=${1:-20}
 
-sudo timeout $t perf record ${=RECORD_OPTS} -g --call-graph=fp -p ${EMACS_PID} -eprobe_libc:{free,{malloc,calloc,realloc}{,_ret}} -eprobe_libc:aligned_alloc{,_1,_ret} -eprobe_libc:posix_memalign{,_ret}
+sudo timeout $t perf record ${=RECORD_OPTS} -g --call-graph=fp -p ${EMACS_PID} -e 'probe_libc:*'
