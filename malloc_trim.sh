c0440b0387db924871f179dae8fe7e49fcc1428f
Dima Kogan <dima@secretsauce.net>
2016-04-04 19:47:01 -0700

    improved some script ui

--- malloc_trim.sh
+++ malloc_trim.sh
@@ -1,4 +1,12 @@
 #!/bin/zsh
 
-source reademacsvar.sh
+# EMACS_PID must be given in the environment, or we'll infer it
+test -n "$EMACS_PID" || source reademacsvar.sh
+
+before=`./show_daemon_usage.sh`
 gdb --batch-silent --eval-command 'print malloc_trim(0)' -p $EMACS_PID
+after=`./show_daemon_usage.sh`
+
+echo "before: $before"
+echo "after: $after"
+echo "freed: $(($before - $after))"
--- show_daemon_usage.sh
+++ show_daemon_usage.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
-source reademacsvar.sh
+# EMACS_PID must be given in the environment, or we'll infer it
+test -n "$EMACS_PID" || source reademacsvar.sh
 
 ps -h -p $EMACS_PID -O rss  | awk '{print $2}'
