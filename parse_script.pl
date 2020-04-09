4e96abf2b286a5b8452ca8b11c748f11397ca5df
Dima Kogan <dima@secretsauce.net>
2020-04-08 17:28:02 -0700

    parse_script.pl works with a bigger codebase
    1. using the bignum flavor of hex(). Otherwise it barfs that I'm giving it
       too-large integers
    
    2. I'm supporting multi-threaded operations. Each PID has its own state
    
    3. Some consistency checks are allowed to fail; that allocation is just skipped.
       This shouldn't happen, but sometimes it does, and I don't know why

--- parse_script.pl
+++ parse_script.pl
@@ -6,53 +6,69 @@ use warnings;
 
 use feature 'say';
 
+use bigint 'hex'; # to use a version of hex() that can handle very wide integers
+                  # I'm going to see
 
-my $Nbytes_allocated = 0;
-my %allocated;
-
-my ($prev_addr, $prev_ret, $prev_type, $prev_realloc0, $prev_realloc0_addr, $prev_realloc0_bytes);
-my $allocating;
+# the context, indexed on the PID. The elements are
+#   $Nbytes_allocated
+#   %allocated;
+#   ($prev_addr, $prev_ret, $prev_type, $prev_realloc0, $prev_realloc0_addr, $prev_realloc0_bytes);
+#   $allocating;
+my %contexts;
 
 
 while(<>)
 {
     next unless /probe_libc:([^:]+)/;
-
     my $type = $1;
-    my $ret = $type =~ /_ret$/;
-    $type =~ s/_(?:ret|[0-9]+)$//;
+    my $ret = $type =~ /_ret/;
+    $type =~ s/_(?:ret(?:__return)?|[0-9]+)$//;
 
+    my ($pid) = /^\S+\s+(\d+)/ or die
+      "Couldn't parse PID from '$_'";
 
-    if ( $ret && !( !$prev_ret && $type eq $prev_type) &&
-         !($prev_realloc0 && $prev_type eq 'malloc' && $prev_ret && $type eq 'realloc') ) {
-        die "$type ret, but prev wasn't a corresponding !ret";
-    }
-    elsif ( !$ret && !$prev_ret &&
-            !($prev_realloc0 && $prev_type eq 'realloc' && !$prev_ret && $type eq 'malloc') &&
-            $. > 1) {
-        die "$type !ret following another !ret";
-    }
-    elsif ( $prev_realloc0 && !($type eq 'malloc' || $type eq 'realloc'))
+    my $newctx = 0;
+    if (!exists $contexts{$pid})
     {
-        die "realloc(0, N) must be followed by malloc(N)";
+        $contexts{$pid} = { Nbytes_allocated => 0,
+                            allocated        => {} };
+        $newctx = 1;
+    }
+    my $ctx = $contexts{$pid};
+
+    if ( $ret && !( !$ctx->{prev_ret} && $type eq $ctx->{prev_type}) &&
+         !($ctx->{prev_realloc0} && $ctx->{prev_type} eq 'malloc' && $ctx->{prev_ret} && $type eq 'realloc') ) {
+
+        # This should never happen, but sometimes it does. So I just ignore it
+        # die "PID $pid: $type ret, but prev wasn't a corresponding !ret";
+        next;
+    }
+    elsif ( !$ret && !$ctx->{prev_ret} &&
+            !($ctx->{prev_realloc0} && $ctx->{prev_type} eq 'realloc' && !$ctx->{prev_ret} && $type eq 'malloc') &&
+            !$newctx) {
+        die "PID $pid: $type !ret following another !ret";
+    }
+    elsif ( $ctx->{prev_realloc0} && !($type eq 'malloc' || $type eq 'realloc'))
+    {
+        die "PID $pid: realloc(0, N) must be followed by malloc(N)";
     }
     elsif ( !$ret )
     {
         if ($type eq 'malloc' && /bytes=([0-9a-z]+)/)
         {
-            $allocating = hex $1;
-            if ( $prev_realloc0 && $allocating != $prev_realloc0_bytes )
+            $ctx->{allocating} = hex $1;
+            if ( $ctx->{prev_realloc0} && $ctx->{allocating} != $ctx->{prev_realloc0_bytes} )
             {
-                die "realloc(0, N) must be followed by malloc(N)";
+                die "PID $pid: realloc(0, N) must be followed by malloc(N)";
             }
         }
         elsif ($type eq 'calloc' && /elem_size=([0-9a-z]+).*n=([0-9a-z]+)/)
         {
-            $allocating = (hex $1) * (hex $2);
+            $ctx->{allocating} = (hex $1) * (hex $2);
         }
         elsif ($type eq 'aligned_alloc' && /bytes=([0-9a-z]+)/)
         {
-            $allocating = hex $1;
+            $ctx->{allocating} = hex $1;
         }
         elsif ($type eq 'realloc' && /oldmem=([0-9a-z]+).*bytes=([0-9a-z]+)/)
         {
@@ -60,27 +76,27 @@ while(<>)
             {
                 # realloc(0, xxx) is always mapped to a malloc apparently. I treat
                 # this specially
-                $prev_realloc0       = 1;
-                $prev_realloc0_bytes = hex $2;
+                $ctx->{prev_realloc0}       = 1;
+                $ctx->{prev_realloc0_bytes} = hex $2;
             }
             else
             {
-                $allocating = hex $2;
-                $prev_addr = $1;
+                $ctx->{allocating} = hex $2;
+                $ctx->{prev_addr} = $1;
             }
         }
         elsif ($type eq 'free' && /mem=([0-9a-z]+)/)
         {
             if ( hex($1) != 0)  # free(0) does nothing
             {
-                if (!defined $allocated{$1})
+                if (!defined $ctx->{allocated}{$1})
                 {
                     say "Unallocated free at $1. Line $.";
                 }
                 else
                 {
-                    $Nbytes_allocated -= $allocated{$1}{bytes};
-                    delete $allocated{$1};
+                    $ctx->{Nbytes_allocated} -= $ctx->{allocated}{$1}{bytes};
+                    delete $ctx->{allocated}{$1};
                 }
             }
 
@@ -96,7 +112,7 @@ while(<>)
     {
         if ( !/arg1=([0-9a-z]+)/ )
         {
-            die "Ret didn't get arg1";
+            die "PID $pid: Ret didn't get arg1";
         }
 
         my $addr = $1;
@@ -108,59 +124,59 @@ while(<>)
         }
         elsif ( $type =~ /^(?:[cm]alloc|aligned_alloc)$/ )
         {
-            if (defined $allocated{$addr})
+            if (defined $ctx->{allocated}{$addr})
             {
                 say "Double alloc at $addr. Line $.";
             }
             else
             {
-                $allocated{$addr}{bytes} = $allocating;
-                $allocated{$addr}{line} = $.;
-                $Nbytes_allocated += $allocating;
+                $ctx->{allocated}{$addr}{bytes} = $ctx->{allocating};
+                $ctx->{allocated}{$addr}{line} = $.;
+                $ctx->{Nbytes_allocated} += $ctx->{allocating};
             }
 
-            if ( $prev_realloc0 && $type eq 'malloc')
+            if ( $ctx->{prev_realloc0} && $type eq 'malloc')
             {
-                $prev_realloc0_addr = $addr;
+                $ctx->{prev_realloc0_addr} = $addr;
             }
         }
         elsif ( $type eq 'realloc' )
         {
-            if ( $prev_realloc0 )
+            if ( $ctx->{prev_realloc0} )
             {
-                if ( $addr ne $prev_realloc0_addr )
+                if ( $addr ne $ctx->{prev_realloc0_addr} )
                 {
-                    die "realloc(0, N) must be followed by malloc(N); differing addr";
+                    die "PID $pid: realloc(0, N) must be followed by malloc(N); differing addr";
                 }
 
-                $prev_realloc0       = undef;
-                $prev_realloc0_addr  = undef;
-                $prev_realloc0_bytes = undef;
+                $ctx->{prev_realloc0}       = undef;
+                $ctx->{prev_realloc0_addr}  = undef;
+                $ctx->{prev_realloc0_bytes} = undef;
             }
             else
             {
-                my $prev0 = (hex($prev_addr) == 0);
+                my $prev0 = (hex($ctx->{prev_addr}) == 0);
 
-                if (!$prev0 && !defined $allocated{$prev_addr})
+                if (!$prev0 && !defined $ctx->{allocated}{$ctx->{prev_addr}})
                 {
-                    say "realloc not alloced at $prev_addr. Line $.";
+                    say "realloc not alloced at $ctx->{prev_addr}. Line $.";
                     $prev0 = 1;
                 }
 
-                if ($addr ne $prev_addr && defined $allocated{$addr})
+                if ($addr ne $ctx->{prev_addr} && defined $ctx->{allocated}{$addr})
                 {
                     say "Double realloc at $addr. Line $.";
                 }
 
                 if ( !$prev0 )
                 {
-                    $Nbytes_allocated -= $allocated{$prev_addr}{bytes};
-                    delete $allocated{$prev_addr};
+                    $ctx->{Nbytes_allocated} -= $ctx->{allocated}{$ctx->{prev_addr}}{bytes};
+                    delete $ctx->{allocated}{$ctx->{prev_addr}};
                 }
 
-                $allocated{$addr}{bytes} = $allocating;
-                $allocated{$addr}{line} = $.;
-                $Nbytes_allocated += $allocating;
+                $ctx->{allocated}{$addr}{bytes} = $ctx->{allocating};
+                $ctx->{allocated}{$addr}{line} = $.;
+                $ctx->{Nbytes_allocated} += $ctx->{allocating};
             }
         }
         else
@@ -170,22 +186,28 @@ while(<>)
         }
 
 
-        $allocating = undef;
+        $ctx->{allocating} = undef;
     }
 
-
-    $prev_type = $type;
-    $prev_ret = $ret;
+    $ctx->{prev_type} = $type;
+    $ctx->{prev_ret}  = $ret;
 }
 
 
-$Nbytes_allocated /= 1e6;
-say "Total allocated: $Nbytes_allocated MB";
-say '';
-
-for my $addr ( sort { $allocated{$a}{line} <=> $allocated{$b}{line}} keys %allocated )
+for my $pid ( keys %contexts )
 {
-    my ($bytes,$line) = ($allocated{$addr}{bytes},
-                         $allocated{$addr}{line});
-    say "Leaked " . sprintf('%5d', $bytes) . " bytes at line $line ($addr)";
+    my $ctx = $contexts{$pid};
+
+    say "============== PID $pid =============";
+
+    $ctx->{Nbytes_allocated} /= 1e6;
+    say "Total allocated: $ctx->{Nbytes_allocated} MB";
+    say '';
+
+    for my $addr ( sort { $ctx->{allocated}{$a}{line} <=> $ctx->{allocated}{$b}{line}} keys %{$ctx->{allocated}} )
+    {
+        my ($bytes,$line) = ($ctx->{allocated}{$addr}{bytes},
+                             $ctx->{allocated}{$addr}{line});
+        say "Leaked " . sprintf('%5d', $bytes) . " bytes at line $line ($addr)";
+    }
 }
